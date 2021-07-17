079aeddb550bfba1e573278791b227fd734cbd4d
Robert John <WaveShapePlay@gmail.com>
2021-07-17 06:41:52 -0400

    Cleaned up scpi class and data format

--- 2831E_DMM_DataLogging.py
+++ 2831E_DMM_DataLogging.py
@@ -9,11 +9,12 @@ class Scpi_CMD():
     def __init__(self):
         self.current_index = 0
         self.dataDict = {}
-        
+        self.numSamples = 10
+    
     def getInfo(self):
         ser.write(b'*idn?\n')
         print(ser.readline())
-
+    
     def mode_volt_dc(self): 
         ser.write(b'func volt:dc\n')
         ser.write(b'volt:dc:rang:auto 1\n')
@@ -26,14 +27,18 @@ class Scpi_CMD():
     def getScpiData(self):
         ser.write(b'fetch?\n')
         self.val = ser.readline()
+
         if self.val != b'fetch?\n':
             self.val = self.val.decode()
             self.val = str(self.val)
-            self.val = self.val[0:-3]
-            self.current_index += 1
-            self.formatDataDict()
-            data = self.formatDataDict()
-            #print(data)
+            try:
+                self.val = float(self.val)
+                print(self.val)
+                self.current_index += 1
+                self.formatDataDict()
+                data = self.formatDataDict()
+            except:
+                pass
 
     def formatDataDict(self):
         self.dataDict[self.current_index] = self.val
@@ -43,9 +48,8 @@ class Scpi_CMD():
 scpi = Scpi_CMD()
 scpi.getInfo()
 scpi.mode_volt_dc()
-numSamples = 20
 
-for i in range(0,numSamples):
+while(scpi.current_index < scpi.numSamples):
     scpi.getScpiData()
 
 print(scpi.dataDict)
